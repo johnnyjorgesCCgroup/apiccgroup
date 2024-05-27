a7412f7ec4b41cc0b6f5bab580b7904525e913b9
johnny jorgesvega <johnny.ccgroup@gmail.com>
2024-05-27 17:21:08 -0500

    27_05a

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,19 +1,19 @@
 module.exports = {
-    apps: [
-      {
-        name: 'my-app',
-        script: 'app.js',
-        instances: 1,
-        autorestart: true,
-        watch: false,
-        max_memory_restart: '1G',
-        env: {
-          NODE_ENV: 'development'
-        },
-        env_production: {
-          NODE_ENV: 'production'
-        }
+  apps: [
+    {
+      name: 'my-app',
+      script: 'app.js',
+      instances: 'max', // utiliza todos los núcleos disponibles
+      exec_mode: 'cluster', // usa el modo cluster para aprovechar múltiples núcleos
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '2G',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
       }
-    ]
-  };
-  
+    }
+  ]
+};
